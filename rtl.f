cca41376723894c3f3f306b0926bac7bb68abccd
Kiet Le <ktlbusiness11@gmail.com>
2026-04-15 22:31:49 -0400

    REPO_INIT: init repo with submodules and core rtl Add submodules for build-tools, core-control, hash-sampler, poly-arith, poly-mem, and transcoder units. Add base RTL (qrem_core, qrem_pkg). Add filelist (rtl.f) and CI workflow.

---
+++ rtl.f
@@ -0,0 +1,11 @@
+# 1. Submodules (i.e. -f lib/keccak-fips202-sv/rtl.f)
+-f lib/hash-sampler-unit/rtl.f
+-f lib/poly-arith-unit/rtl.f
+-f lib/poly-mem-subsystem/rtl.f
+-f lib/transcoder-unit/rtl.f
+
+# 2. Local Packages (i.e., rtl/my_pkg.sv)
+-f rtl/qrem_pkg.sv
+
+# 3. Local RTL (i.e., rtl/transcoder_unit.sv)
+-f rtl/qrem_core.sv
---
+++ rtl/qrem_core.sv
@@ -0,0 +1,11 @@
+/*
+ * Module Name: qrem_core
+ * Author(s): Mai Komar, Kiet Le
+ * Description:
+ */
+
+import qrem_pkg::*;
+
+module qrem_core (
+
+endmodule
