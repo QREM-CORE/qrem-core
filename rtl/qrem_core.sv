8effb2c30fd7050c0052fd321684963061d347f4
Kiet Le <ktlbusiness11@gmail.com>
2026-04-22 16:55:57 -0400

    Add common-rtl submodule for global package and Update Submodules  (#2)
    * Add common-rtl submodule and update other submodules
    
    * Add common-rtl to rtl.f
    
    * Update submodule file
    
    * Use only fpga for top level
    
    * Fix rtl.f
    
    * Import global package
    
    * Revise naming in top level
    
    * Edit header comment again
    
    * Update all sub modules

--- rtl/qrem_core.sv
+++ rtl/qrem_core.sv
@@ -1,9 +1,17 @@
 /*
  * Module Name: qrem_core
- * Author(s): Kiet Le, Quardin Lyttle, Jessica Buentipo, Mai Komar, Salwan Aldhahab, Mavra Muzmmal
+ * Author(s):
+        York University - Lassonde School of Engineering - 2026 Capstone - Team 32
+            - Salwan Aldhahab
+            - Jessica Buentipo
+            - Mai Komar
+            - Kiet Le
+            - Quardin Lyttle
+            - Mavra Muzmmal
  * Description:
  */
 
+import qrem_global_pkg::*;
 import qrem_pkg::*;
 
 module qrem_core ();
